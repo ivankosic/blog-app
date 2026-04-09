0098da151093474b077a84e33794fbfa05a47d7c
ivankosic <ivankosic55@gmail.com>
2026-04-09 20:01:55 +0200

    Split schemas into response and create

--- app/schemas.py
+++ app/schemas.py
@@ -1,15 +1,26 @@
 from pydantic import BaseModel
 import datetime
 
-class BlogPost(BaseModel):
+class BlogPostResponse(BaseModel):
     id : int
     title : str
     content : str
     created_at : datetime
 
-class Comment(BaseModel):
+class BlogPostCreate(BaseModel):
+    title : str
+    content : str
+
+
+
+class CommentResponse(BaseModel):
     id : int
     blog_id : int
     author : str
     content : str
     created_at : datetime
+
+class CommentCreate(BaseModel):
+    blog_id : int
+    author : str
+    content : str
