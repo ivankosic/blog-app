5cc88cba5566c56fad5b6d396cc8480853e0ffb5
ivankosic <ivankosic55@gmail.com>
2026-04-10 22:32:24 +0200

    Modified models

--- app/models.py
+++ app/models.py
@@ -2,16 +2,16 @@ from sqlalchemy import Column, ForeignKey, Integer, String, DateTime
 from .database import Base
 class BlogPosts(Base):
     __tablename__ = 'blog_posts'
-    id = Column(Integer, primary_key=True, index=True)
-    title = Column(String, index=True)
-    content = Column(String, index=True)
+    id = Column(Integer, primary_key=True, index=True, autoincrement=True)
+    title = Column(String)
+    content = Column(String)
     created_at = Column(DateTime)
 
 
 class Comments(Base):
     __tablename__ = 'comments'
-    id = Column(Integer,primary_key=True, index=True)
+    id = Column(Integer,primary_key=True, index=True, autoincrement=True)
     blog_id = Column(Integer, ForeignKey("blog_posts.id"))
-    author = Column(String, index=True)
-    content = Column(String, index=True)
+    author = Column(String)
+    content = Column(String)
     created_at = Column(DateTime)
